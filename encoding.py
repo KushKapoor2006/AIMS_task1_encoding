196a5c96459779ef44127993a967da8f4fe36753
Kush Kapoor <32592361+KushKapoor2006@users.noreply.github.com>
2024-10-23 22:30:06 +0530

    Add files via upload

---
+++ encoding.py
@@ -0,0 +1,56 @@
+import pandas as pd
+
+painting_data = pd.read_csv('painting_data.csv')
+
+def ordinal(painting_data):   
+    cat_col = []
+    for i in range(len(painting_data.columns)):
+        cat_col.append(i)
+
+    uni_cat_col = []
+    for a in range(len(cat_col)):
+        col = cat_col[a]
+        uni_cat_col.append(painting_data[col].unique())
+
+    
+    num_val = [j for j in range(len(uni_cat_col))]
+
+    for k in range(len(uni_cat_col)):
+        painting_data[col] = painting_data[col].replace(uni_cat_col[k], num_val[k])
+
+    print(painting_data)
+
+
+
+def one_hot(painting_data):
+    cat_cols_1 = []
+    for col_1 in range(len(painting_data.columns)):
+            cat_cols_1.append(col_1)
+   
+    for i in range(len(cat_cols_1)) :
+        col = cat_cols_1[i]
+        lst = list(painting_data[col].unique())
+
+     
+        for j in range(len(lst)) :
+            for k in range(len(painting_data[col_1])) :
+                if lst[j] == painting_data[cat_cols_1[i]].iloc[k] :
+                    painting_data.loc[painting_data.index[k], lst[j]] = 1
+
+                else :
+                    painting_data.loc[painting_data.index[k], lst[j]] = 0
+
+
+    painting_data.drop(columns = cat_cols_1 , inplace = True)
+    print(painting_data)
+
+
+print(f"Orignal Dataset is: ",painting_data)
+a = int(input("\nEnter 1 for Ordinal, 2 for One_Hot encoding: "))
+
+if(a==1):
+    ordinal(painting_data)
+elif(a==2):
+    one_hot(painting_data)
+else:
+    print("Invalid input")
